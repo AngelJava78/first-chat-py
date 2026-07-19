0613b505dc077ad1e97ac729c6bfd6feb3222aca
Angel Javier Valdez <angel_java@hotmail.com>
2026-07-19 05:26:38 +0000

    Iteration added

--- app.py
+++ app.py
@@ -8,10 +8,23 @@ client = OpenAI(
     base_url=endpoint,
     api_key=token_provider
 )
-response = client.responses.create(
-    model= deployment_name,
-    input="Quién ganó el 3er. lugar del mundial de futbol 2026. https://www.telemundo.com/noticias/noticias-telemundo/internacional/live-blog/mundial-2026-francia-inglaterra-partido-tercer-lugar-rcna588179"
-)
+# response = client.responses.create(
+#     model= deployment_name,
+#     input="Quién ganó el 3er. lugar del mundial de futbol 2026. https://www.telemundo.com/noticias/noticias-telemundo/internacional/live-blog/mundial-2026-francia-inglaterra-partido-tercer-lugar-rcna588179"
+# )
 
-answer = response.output_text;
-print(f"Answer: {answer}")
+# answer = response.output_text;
+# print(f"Answer: {answer}")
+
+while True:
+    input_text = input("Question: ")
+    if input_text.lower() == "exit":
+        break
+
+    response = client.responses.create(
+        model= deployment_name,
+        input=input_text
+    )
+    print("Answer:")
+    print(response.output_text)
+
