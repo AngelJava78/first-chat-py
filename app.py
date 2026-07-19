27aa049e0c4ed61e6c701863a413fed4bc26bc3d
Angel Javier Valdez <angel_java@hotmail.com>
2026-07-18 22:32:41 -0600

    Chat completed

---
+++ app.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+endpoint = "https://prj-ai103-dev-eu2-resource.services.ai.azure.com/openai/v1"
+deployment_name = "gpt-5-mini"
+azure_endpoint="https://ai.azure.com/.default"
+token_provider=get_bearer_token_provider(DefaultAzureCredential(),azure_endpoint)
+client = OpenAI(
+    base_url=endpoint,
+    api_key=token_provider
+)
+response = client.responses.create(
+    model= deployment_name,
+    input="Quién ganó el 3er. lugar del mundial de futbol 2026. https://www.telemundo.com/noticias/noticias-telemundo/internacional/live-blog/mundial-2026-francia-inglaterra-partido-tercer-lugar-rcna588179"
+)
+
+answer = response.output_text;
+print(f"Answer: {answer}")
